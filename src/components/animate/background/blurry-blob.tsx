46da3e395c46327b7280aacf152b9bc0ea9edec1
飯森毅 <iiyotu1142@icloud.com>
2024-09-23 20:33:52 +0900

    一旦コミット

--- src/components/animate/background/blurry-blob.tsx
+++ src/components/animate/background/blurry-blob.tsx
@@ -12,7 +12,6 @@ export default function BlurryBlob({
   return (
     <div className="min-h-52 min-w-52 flex items-center justify-center fixed top-0 left-0 w-full h-screen z-[-1]">
       <div className="relative w-full max-w-lg flex items-center justify-center">
-        {/* 1つ目のblob */}
         <div
           className={cn(
             "absolute h-72 w-72 animate-pop-blob rounded-full bg-blue-400 p-8 opacity-45 mix-blend-multiply blur-3xl filter",
@@ -21,7 +20,6 @@ export default function BlurryBlob({
             firstBlobColor
           )}
         />
-        {/* 2つ目のblob */}
         <div
           className={cn(
             "absolute h-72 w-72 animate-pop-blob rounded-full bg-purple-400 p-8 opacity-45 mix-blend-multiply blur-3xl filter",
