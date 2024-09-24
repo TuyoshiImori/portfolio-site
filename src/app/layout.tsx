8ff1b9548f058a7f8adb04880ef8bdf85fd33285
飯森毅 <iiyotu1142@icloud.com>
2024-09-24 21:06:21 +0900

    blobの表示位置を調整

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,11 +50,9 @@ export default function RootLayout({
         >
           <div className="relative">
             <div>
-              <BlurryBlob
-                className=""
-                firstBlobColor="bg-blue-400"
-                secondBlobColor="bg-purple-400"
-              />
+              <BlurryBlob className="bottom-0 left-0 bg-blue-400 absolute" />
+              <BlurryBlob className="top-0 right-0 bg-green-400 absolute" />
+              <BlurryBlob className="bg-purple-400" />
               <Header />
               <div className="container mx-auto px-5">
                 {children}
--- src/components/animate/background/blurry-blob.tsx
+++ src/components/animate/background/blurry-blob.tsx
@@ -1,31 +1,14 @@
 import { cn } from "@/lib/utils";
-interface BlobProps extends React.HTMLAttributes<HTMLDivElement> {
-  firstBlobColor: string;
-  secondBlobColor: string;
-}
+interface BlobProps extends React.HTMLAttributes<HTMLDivElement> {}
 
-export default function BlurryBlob({
-  className,
-  firstBlobColor,
-  secondBlobColor,
-}: BlobProps) {
+export default function BlurryBlob({ className }: BlobProps) {
   return (
-    <div className="min-h-52 min-w-52 flex items-center justify-center fixed top-0 left-0 w-full h-screen z-[-1]">
-      <div className="relative w-full max-w-lg flex items-center justify-center">
+    <div className="flex items-center justify-center fixed w-full h-screen z-[-1]">
+      <div className="relative w-full h-full flex items-center justify-center">
         <div
           className={cn(
-            "absolute h-72 w-72 animate-pop-blob rounded-full bg-blue-400 p-8 opacity-45 mix-blend-multiply blur-3xl filter",
-            "bottom-0 left-0 translate-x-1/2 translate-y-1/2",
-            className,
-            firstBlobColor
-          )}
-        />
-        <div
-          className={cn(
-            "absolute h-72 w-72 animate-pop-blob rounded-full bg-purple-400 p-8 opacity-45 mix-blend-multiply blur-3xl filter",
-            "top-0 right-0 -translate-x-1/2 -translate-y-1/2",
-            className,
-            secondBlobColor
+            "h-72 w-72 animate-pop-blob rounded-full p-8 opacity-45 mix-blend-multiply blur-3xl filter",
+            className
           )}
         />
       </div>
