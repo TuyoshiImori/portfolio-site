6a4fa58d98ac70a0bab1032ada675f1305ffd1fe
飯森毅 <iiyotu1142@icloud.com>
2024-09-29 20:55:13 +0900

    headerを上に固定、react-iconsを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,11 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import "./globals.css";
-import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { cn } from "@/libs/utils";
 import { ThemeProvider } from "next-themes";
 import BlurryBlob from "@/components/animate/background/blurry-blob";
+import { Header } from "@/components/Header";
 const fontSans = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 // const geistSans = localFont({
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -43,7 +43,8 @@ export const Navigation: FunctionComponent = () => {
 
 export const Header: FunctionComponent = () => {
   return (
-    <header className="absolute z-10 w-full top-0 px-20 py-4">
+    // headerにstickyを追加して常に上に固定されるようにする
+    <header className="sticky z-10 w-full top-0 px-20 py-4">
       <section
         className={`flex items-center justify-between px-12 py-4 rounded-full ${glassmorphism}`}
       >
