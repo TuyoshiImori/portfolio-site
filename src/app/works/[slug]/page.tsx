8be797f5035ad8e120bd05e40c5cf78914407fa2
飯森毅 <iiyotu1142@icloud.com>
2024-10-09 20:28:33 +0900

    マークダウンをちゃんと表示できるようにした

--- src/app/works/[slug]/page.tsx
+++ src/app/works/[slug]/page.tsx
@@ -6,8 +6,7 @@ import Image from "next/image";
 import imageSize from "image-size";
 import rehypeRaw from "rehype-raw";
 import remarkGfm from "remark-gfm";
-import Link from "next/link";
-import Markdown from "react-markdown";
+import ReactMarkdown from "react-markdown";
 
 export default function Home({ params }: { params: { slug: string } }) {
   const { slug } = params;
@@ -24,6 +23,7 @@ export default function Home({ params }: { params: { slug: string } }) {
         ? image
         : "/works/" + image
       : image;
+
   return (
     <div className="flex flex-col justify-center items-center justify-items-center">
       <div className=" items-center">
@@ -46,66 +46,24 @@ export default function Home({ params }: { params: { slug: string } }) {
         <div className="text-base font-bold mt-4">{title}</div>
         <div className="text-base font-bold mt-4">{date}</div>
         <div className="text-base font-bold mt-4">{tags}</div>
-        <Markdown
+        <ReactMarkdown
           rehypePlugins={[rehypeRaw]}
           remarkPlugins={[remarkGfm]}
           components={{
-            h1: "h2",
-            h2: "h3",
-            h3: "h4",
-            a(props) {
-              const { href, children } = props;
-              return href?.startsWith("/") ? (
-                <Link href={href}>{children}</Link>
-              ) : (
-                <a href={href} rel="noreferrer" target="_blank">
-                  {children}
-                </a>
-              );
-            },
-            img(props) {
-              const { src, alt, width, height } = props;
-              if (!src) return <span>src が指定されていません。</span>;
-
-              const image =
-                process.env.NODE_ENV === "development"
-                  ? src?.startsWith("/")
-                    ? src
-                    : "/works/" + src
-                  : src;
-
-              return (
-                <>
-                  <Image
-                    alt={alt ?? "alt なし"}
-                    src={image}
-                    height={
-                      height
-                        ? Number(height)
-                        : imageSize(
-                            process.env.NODE_ENV === "development"
-                              ? "public" + imagePath
-                              : imagePath
-                          ).height
-                    }
-                    width={
-                      width
-                        ? Number(width)
-                        : imageSize(
-                            process.env.NODE_ENV === "development"
-                              ? "public" + imagePath
-                              : imagePath
-                          ).width
-                    }
-                  />
-                  {alt && <span>{alt}</span>}
-                </>
-              );
-            },
+            h1: (props) => <h1 className="text-3xl font-bold" {...props} />,
+            h2: (props) => <h2 className="text-2xl font-bold" {...props} />,
+            h3: (props) => <h3 className="text-xl font-bold" {...props} />,
+            p: (props) => <p className="my-4" {...props} />,
+            a: (props) => <a className="text-blue-600 underline" {...props} />,
+            ul: (props) => <ul className="list-disc list-inside" {...props} />,
+            ol: (props) => (
+              <ol className="list-decimal list-inside" {...props} />
+            ),
+            li: (props) => <li className="my-2" {...props} />,
           }}
         >
           {content}
-        </Markdown>
+        </ReactMarkdown>
       </div>
     </div>
   );
